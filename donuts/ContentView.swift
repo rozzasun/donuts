276ba36bdebde2b0011e40be012b09f09d5cedc9
Rosa Sun <rosa.sun@shopify.com>
2020-10-01 17:24:13 -0400

    Initial Commit

---
+++ donuts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  donuts
+//
+//  Created by Rosa Sun on 2020-10-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ donuts/donutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  donutsApp.swift
+//  donuts
+//
+//  Created by Rosa Sun on 2020-10-01.
+//
+
+import SwiftUI
+
+@main
+struct donutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
